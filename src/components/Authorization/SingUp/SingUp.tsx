79e49ba99bcfde8f39ee80b8fec815d3cfd55b2f
Daniil <prestig.rd85@gmail.com>
2021-08-25 19:48:15 +0300

    regEx

--- src/components/Authorization/SingUp/SingUp.tsx
+++ src/components/Authorization/SingUp/SingUp.tsx
@@ -30,44 +30,44 @@ const SignUp: React.FC = () => {
         dispatch(signUpThunk(value));
     };
 
-    return (<div className={classNames(styles.signIn)}>
-        <div className={classNames(styles.signIn__wrapper)}>
+    return (<div className={classNames(styles.signUp)}>
+        <div className={classNames(styles.signUp__wrapper)}>
             <Formik
                 initialValues={initialValues}
                 validate={signUpFormValidation}
                 onSubmit={formSubmit}>
-                <Form className={classNames(styles.signIn__form)}>
-                    <div className={classNames(styles.signIn__filed)}>
-                        <label className={classNames(styles.signIn__label)} htmlFor="name">Your email: </label>
-                        <Field className={classNames(styles.signIn__input)} id="name" name="name" type="text"
+                <Form className={classNames(styles.signUp__form)}>
+                    <div className={classNames(styles.signUp__filed)}>
+                        <label className={classNames(styles.signUp__label)} htmlFor="name">Your name: </label>
+                        <Field className={classNames(styles.signUp__input)} id="name" name="name" type="text"
                                placeholder="Name"/>
-                        <ErrorMessage className={classNames(styles.signIn__error)} component="div" name="name"/>
+                        <ErrorMessage className={classNames(styles.signUp__error)} component="div" name="name"/>
                     </div>
 
-                    <div className={classNames(styles.signIn__filed)}>
-                        <label className={classNames(styles.signIn__label)} htmlFor="email">Your email: </label>
-                        <Field className={classNames(styles.signIn__input)} id="email" name="email" type="email"
+                    <div className={classNames(styles.signUp__filed)}>
+                        <label className={classNames(styles.signUp__label)} htmlFor="email">Your email: </label>
+                        <Field className={classNames(styles.signUp__input)} id="email" name="email" type="email"
                                placeholder="Email"/>
-                        <ErrorMessage className={classNames(styles.signIn__error)} component="div" name="email"/>
+                        <ErrorMessage className={classNames(styles.signUp__error)} component="div" name="email"/>
                     </div>
 
-                    <div className={classNames(styles.signIn__filed)}>
-                        <label className={classNames(styles.signIn__label)} htmlFor="password">Your password: </label>
-                        <Field className={classNames(styles.signIn__input)} id="password" name="password"
+                    <div className={classNames(styles.signUp__filed)}>
+                        <label className={classNames(styles.signUp__label)} htmlFor="password">Your password: </label>
+                        <Field className={classNames(styles.signUp__input)} id="password" name="password"
                                type="password" placeholder="Password"/>
-                        <ErrorMessage className={classNames(styles.signIn__error)} component="div" name="password"/>
+                        <ErrorMessage className={classNames(styles.signUp__error)} component="div" name="password"/>
                     </div>
 
-                    <div className={classNames(styles.signIn__filed)}>
-                        <label className={classNames(styles.signIn__label)} htmlFor="repeatPassword">Repeat your password: </label>
-                        <Field className={classNames(styles.signIn__input)} id="repeatPassword" name="repeatPassword"
+                    <div className={classNames(styles.signUp__filed)}>
+                        <label className={classNames(styles.signUp__label)} htmlFor="repeatPassword">Repeat your password: </label>
+                        <Field className={classNames(styles.signUp__input)} id="repeatPassword" name="repeatPassword"
                                type="password" placeholder="Password"/>
-                        <ErrorMessage className={classNames(styles.signIn__error)} component="div" name="password"/>
+                        <ErrorMessage className={classNames(styles.signUp__error)} component="div" name="password"/>
                     </div>
 
-                    <div className={classNames(styles.signIn__submit)}>
-                        <button className={classNames(styles.signIn__btn)} type="submit">Sign up</button>
-                        <div className={classNames(styles.signIn__status)}>
+                    <div className={classNames(styles.signUp__submit)}>
+                        <button className={classNames(styles.signUp__btn)} type="submit">Sign up</button>
+                        <div className={classNames(styles.signUp__status)}>
                             {formData.fetching ? <img src={formPreloader} alt=""/> : <span>{formData.error}</span>}
                         </div>
                     </div>
--- src/components/Authorization/formValidation.ts
+++ src/components/Authorization/formValidation.ts
@@ -13,7 +13,7 @@ export const signInFormValidation = (values: ISingInFormValues): ISingInFormErro
 
     if (!values.password) errors.password = 'filed is required';
     else if (values.password.length < 4) errors.password = 'password must be more than 4 symbols';
-
+    else if (!/^[A-Z0-9._-]{4,}$/i.test(values.password)) errors.password = 'incorrect password';
 
     return errors;
 };
@@ -22,12 +22,15 @@ export const signUpFormValidation = (values: ISingUpFormValues): ISingUpFormErro
     const errors: ISingUpFormErrors = {};
 
     if (!values.name) errors.name = 'filed is required';
+    else if (!/^[A-Z\s]{1,}$/i.test(values.name)) errors.name = 'incorrect name';
+
 
     if (!values.email) errors.email = 'filed is required';
     else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) errors.email = 'incorrect email';
 
     if (!values.password) errors.password = 'filed is required';
     else if (values.password.length < 4) errors.password = 'password must be more than 4 symbols';
+    else if (!/^[A-Z0-9._-]{4,}$/i.test(values.password)) errors.password = 'incorrect password';
 
     if (values.password !== values.repeatPassword) errors.password = 'password mismatch';
 
